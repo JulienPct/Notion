d81986e33a4ecf61b98a0b75cc528fec9f622158
Julien Pactat <julien.pactat.akyos@gmail.com>
2023-10-25 16:26:21 +0200

    Feat: Add new note creation

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { PlusCircle } from "lucide-react";
-import Image from "next/image";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Sans titre" });
+
+    toast.promise(promise, {
+      loading: "Création d'une nouvelle note...",
+      success: "Nouvelle note créée !",
+      error: "Échec de la création d'une nouvelle note.",
+    });
+  }
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -27,7 +42,7 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-medium">
         Bienvenue dans le Notion de {user?.firstName}
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Créer une note
       </Button>
