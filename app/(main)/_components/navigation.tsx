bbc1cd0c2463114e8765b7b2f36ad949dff9e400
Julien Pactat <julien.pactat.akyos@gmail.com>
2023-11-03 14:25:57 +0100

    Feat: Add recursive note creation

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,18 +1,27 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeftIcon, MenuIcon } from "lucide-react";
+import {
+  ChevronsLeftIcon,
+  MenuIcon,
+  PlusCircle,
+  Search,
+  Settings,
+} from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
-  const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -95,6 +104,16 @@ export const Navigation = () => {
     }
   };
 
+  const handleCreate = () => {
+    const promise = create({ title: "Sans titre" });
+
+    toast.promise(promise, {
+      loading: "Création d'une nouvelle note...",
+      success: "Nouvelle note créée !",
+      error: "Échec de la création d'une nouvelle note.",
+    });
+  };
+
   return (
     <>
       <aside
@@ -117,11 +136,16 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item label="Recherche" icon={Search} isSearch onClick={() => {}} />
+          <Item label="Paramètres" icon={Settings} onClick={() => {}} />
+          <Item
+            onClick={handleCreate}
+            label="Nouvelle page"
+            icon={PlusCircle}
+          />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
