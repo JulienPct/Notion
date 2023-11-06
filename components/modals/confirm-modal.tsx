bedfcda4789a068e58ac4e26769cae1d7b48e116
Julien Pactat <julien.pactat.akyos@gmail.com>
2023-11-06 17:43:59 +0100

    FEAT: Add archiving and removing actions

---
+++ components/modals/confirm-modal.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+}
+
+export const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
+  const handleConfirm = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    onConfirm();
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger onClick={(e) => e.stopPropagation()} asChild>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Êtes-vous sûr ?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Cette action est irréversible.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={(e) => e.stopPropagation()}>
+            Annuler
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={handleConfirm}>
+            Confirmer
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
